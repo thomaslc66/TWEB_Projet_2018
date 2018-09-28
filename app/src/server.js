9382f4ab2715b1db83dd83f8c33db1730160b9d5
Thomas Léchaire <thomas@office.practeo.ch>
2018-09-28 10:37:14 +0200

    Eslint install

--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,13 @@
 // server.js
 const http = require('http');
-const port = 8080;
+const port = 9090;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world!');
 });
+
+
 // Start the server
 server.listen(port, () => {
   console.log(`Magic happens at http://localhost:${port}`);
